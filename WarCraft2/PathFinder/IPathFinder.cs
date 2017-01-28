d8117f735cb66638b5b7b55f466addcb96ab8769
Artmv32 <andrey.artmv@gmail.com>
2017-01-28 14:05:31 -0800

    Added source code for Jump Point Search.

---
+++ WarCraft2/PathFinder/IPathFinder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace WarCraft2.PathFinder
+{
+    public enum MapCell : byte
+    {
+        Free,
+        Obstacle,
+    }
+
+    public interface IPathFinder
+    {
+        int Width { get; }
+
+        int Height { get; }
+
+        List<Point> FindRoute(Point a, Point b);
+
+        void Initialize(MapCell[,] mapInfo);
+
+        bool IsFree(Point p);
+    }
+}
---
+++ WarCraft2/PathFinder/PathFinder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using EpPathFinding.cs;
+
+namespace WarCraft2.PathFinder
+{
+    public class PathFinder : IPathFinder
+    {
+        JumpPointParam jumpParam;
+
+
+
+        public int Height
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+       
+
+        public List<Point> FindRoute(Point a, Point b)
+        {
+            jumpParam.Reset(a, b);
+            List<Point> resultList = JumpPointFinder.FindPath(jumpParam);
+            return resultList;
+        }
+
+        public void Initialize(MapCell[,] mapInfo)
+        {
+            int width = mapInfo.GetLength(0);
+            int height = mapInfo.GetLength(1);
+            var searchGrid = new StaticGrid(width, height);
+            jumpParam = new JumpPointParam(searchGrid, true, true, true, HeuristicMode.EUCLIDEAN);//new JumpPointParam(searchGrid, startPos, endPos, cbCrossCorners.Checked, HeuristicMode.EUCLIDEANSQR);
+            jumpParam.UseRecursive = false;
+        }
+
+        public bool IsFree(Microsoft.Xna.Framework.Point p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
